a01f1a259671b38f7d7c620d1db4b206237af7d3
Rewolve <meticod09@gmail.com>
2025-11-06 11:31:17 +0330

    Added other info

--- info.py
+++ info.py
@@ -4,7 +4,7 @@ API_TOKEN =  os.environ.get("API_TOKEN")
 ADMINGROUP = (os.environ.get("ADMINGROUP")) 
 
 adminlist = {
-        int(os.environ.get("OWNERID")) : {'acceptedorders': 0, 'phonenumber' : "+989029396145", 'order' : {'active_order' : False, 'order_for_userid' : None, 'orderid' : None}}
+        int(os.environ.get("OWNERID")) : {'acceptedorders': 0, 'phonenumber' : "+989000000000", 'order' : {'active_order' : False, 'order_for_userid' : None, 'orderid' : None}}
 }
 
 
